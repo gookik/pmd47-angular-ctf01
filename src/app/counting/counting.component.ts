8f54caa2c34b5407042726420c62ec0fdb449f5b
Patcharin <gookikps@gmail.com>
2021-08-19 23:08:25 +0700

    commit

---
+++ src/app/counting/counting.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+selector: 'app-counting',
+templateUrl: './counting.component.html'
+})
+export class CountingComponent {
+constructor() {}
+}
