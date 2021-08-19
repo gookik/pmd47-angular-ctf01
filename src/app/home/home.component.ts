79f974fd01a7e016471c7504a40360917974d5b6
Patcharin <gookikps@gmail.com>
2021-08-19 18:22:41 +0700

    commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,5 +7,10 @@ import { Component, Input } from '@angular/core';
 export class HomeComponent {
     @Input()
     title!: string;
+
+    count = 0;
     constructor() {}
+
+    countNumber(){this.count = this.count +1;}
+
 }
